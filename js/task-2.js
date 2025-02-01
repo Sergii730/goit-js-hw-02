1704059266ab166ff1e6e88cda76eba2c97ad538
Sergii <batista730tanya@gmail.com>
2025-02-01 11:37:35 +0200

    add work 2.1

--- js/task-2.js
+++ js/task-2.js
@@ -3,16 +3,9 @@ function formatMessage(message, maxLength) {
     ? message
     : message.slice(0, maxLength) + '...';
 }
-formatMessage('Curabitur ligula sapien', 16);
-formatMessage('Curabitur ligula sapien', 23);
-formatMessage('Vestibulum facilisis purus nec', 20);
-formatMessage('Vestibulum facilisis purus nec', 30);
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 15);
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 41);
-
-console.log(formatMessage('Curabitur ligula sapien', 16));
-console.log(formatMessage('Curabitur ligula sapien', 23));
-console.log(formatMessage('Vestibulum facilisis purus nec', 20));
-console.log(formatMessage('Vestibulum facilisis purus nec', 30));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,13 @@
 function checkForSpam(message) {
   return message.toLowerCase().includes('spam') ||
     message.toLowerCase().includes('sale')
-    ? 'true'
-    : 'false';
+    ? true
+    : false;
 }
-checkForSpam('Latest technology news');
-checkForSpam('JavaScript weekly newsletter');
-checkForSpam('Get best sale offers now!');
-checkForSpam('Amazing SalE, only tonight!');
-checkForSpam('Trust me, this is not a spam message');
-checkForSpam('Get rid of sPaM emails. Our book in on sale!');
-checkForSpam('[SPAM] How to earn fast money?');
-
-console.log(checkForSpam('Latest technology news'));
-console.log(checkForSpam('JavaScript weekly newsletter'));
-console.log(checkForSpam('Get best sale offers now!'));
-console.log(checkForSpam('Amazing SalE, only tonight!'));
-console.log(checkForSpam('Trust me, this is not a spam message'));
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
-console.log(checkForSpam('[SPAM] How to earn fast money?'));
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
